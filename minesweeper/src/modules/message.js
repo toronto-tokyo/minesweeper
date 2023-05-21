36220e47c0cc8a28ce00193b50de75c1fd5af009
Minchuk Ilya <cacatubutyt@gmail.com>
2023-05-21 19:19:34 +0200

    feat: improve message content

--- minesweeper/src/modules/message.js
+++ minesweeper/src/modules/message.js
@@ -1,18 +1,28 @@
 import { steps } from './mine';
 
 class Message {
-  constructor() {
+  constructor(button) {
     this.message = document.createElement('div');
+    this.content = document.createElement('div');
+    this.contentText = document.createElement('p');
+    this.restartBtn = button;
     this.message.className = 'message';
+    this.content.className = 'message__content';
+    this.contentText.className = 'message__content-row';
+    this.restartBtn.className = 'message__btn-new-game';
+    this.content.append(this.contentText);
+    this.content.append(this.restartBtn);
+    this.message.append(this.content);
   }
 
   displayLose() {
-    this.message.innerText = 'Game over. Try again';
+    console.log(this.restartBtn);
+    this.contentText.innerText = 'Game over. Try again';
     document.body.append(this.message);
   }
 
   displayWin(time) {
-    this.message.innerText = `Hooray! You found all mines in ${time} seconds and ${steps} moves`;
+    this.contentText.innerText = `Hooray! You found all mines in ${time} seconds and ${steps} moves`;
     document.body.append(this.message);
   }
 }
