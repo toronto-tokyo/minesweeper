1ea8fb7bce091a6e55a8673d6cc6403121833499
Minchuk Ilya <cacatubutyt@gmail.com>
2023-05-22 10:51:21 +0200

    feat: import app into index.js

--- minesweeper/src/index.js
+++ minesweeper/src/index.js
@@ -1,2 +1,9 @@
 import './index.html';
 import './style.scss';
+import App from './modules/app';
+
+const app = new App();
+app.createApp();
+app.restartApp();
+app.changeLvl();
+app.changeMinesCount();
