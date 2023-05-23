1888bbca962fcd75774c39d6ab67baf994d24c23
Minchuk Ilya <cacatubutyt@gmail.com>
2023-05-23 05:18:25 +0200

    feat: add description information on displays

--- minesweeper/src/modules/display.js
+++ minesweeper/src/modules/display.js
@@ -1,28 +1,29 @@
 class Display {
-  constructor() {
+  constructor(description) {
     this.value = 0;
+    this.description = description;
   }
 
   createDisplay() {
     this.display = document.createElement('div');
     this.display.className = 'display';
-    this.display.innerText = this.value;
+    this.display.innerHTML = `<span>${this.description}</span><br><span>${this.value}</span>`;
     return this.display;
   }
 
   setValue(value) {
     this.value = value;
-    this.display.innerText = this.value;
+    this.display.innerHTML = `<span>${this.description}</span><br><span>${this.value}</span>`;
   }
 
   incrValue() {
     this.value += 1;
-    this.display.innerText = this.value;
+    this.display.innerHTML = `<span>${this.description}</span><br><span>${this.value}</span>`;
   }
 
   decrValue() {
     this.value -= 1;
-    this.display.innerText = this.value;
+    this.display.innerHTML = `<span>${this.description}</span><br><span>${this.value}</span>`;
   }
 }
 
